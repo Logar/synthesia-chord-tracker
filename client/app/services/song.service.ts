8dc6dca5f9c236c9367d00543a3b9bb60208693d
Ross Arena <rossarena89@gmail.com>
2020-03-21 22:58:17 -0400

    Improves error handling in the service

--- client/app/services/song.service.ts
+++ client/app/services/song.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { Observable, throwError, of } from 'rxjs';
+import { map, retry, catchError } from 'rxjs/operators';
 
-import { Song } from '../shared/models/song.model';
+import { Chord } from '../shared/models/chord.model';
 
 @Injectable()
 export class SongService {
 
   constructor(private http: HttpClient) { }
-  
-  public getJSON(path: string): Observable<Song[]> {
-    return this.http.get<Song[]>(path)
-                    .map((res:any) => res)
-                    .catch((error:any) => Observable.throw(error))
+
+  public getJSON(path: string): Observable<Chord[]> {
+    const $http = this.http.get<Chord[]>(path);
+
+    return $http
+      .pipe(
+        map((res: any) => {
+          if (!res) {
+            return Observable.throw("Value expected!");
+          }
+          return res;
+        }),
+        catchError(err => {
+          console.log("Caught mapping error and rethrowing", err);
+          return throwError(err);
+        }),
+        catchError(err => {
+          console.log("Caught rethrown error, providing fallback value");
+          return of([]);
+        }),
+        retry(3) // Retry up to 3 times before failing
+      );
   }
 }
