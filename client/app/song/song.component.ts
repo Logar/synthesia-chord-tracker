1e8ccacf52081fb4f3a8c85e0ebddb9c47bc3028
Ross Arena <rossarena89@gmail.com>
2020-03-21 23:02:21 -0400

    Integrates new model logic for a song component

--- client/app/song/song.component.ts
+++ client/app/song/song.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 import { SongService } from '../services/song.service';
 import { Song } from '../shared/models/song.model';
+import { Chord } from '../shared/models/chord.model';
 
 @Component({
   selector: 'app-song',
@@ -11,37 +12,51 @@ import { Song } from '../shared/models/song.model';
 })
 export class SongComponent implements OnInit {
 
-  constructor(private _songService: SongService) { }
-  
   // Access video DOM
   @ViewChild('video') video: ElementRef;
-  // Create a new song model
-  songModel = new Song();
-
+  chordModel: Chord;
+  songModel: Song;
+  songID: number;
+  videoTime: number;
   // Toggle for editing chord data
-  toggleEditMode: boolean = false;
+  toggleEditMode: boolean;
 
-  public ngOnInit(): void {
+  public constructor(
+    private _songService: SongService
+  ) {
+    this.songID = 1;
+    this.songModel = new Song(this.songID, "Yasashia", "Bb Lydian", Array());
+    this.chordModel = new Chord(1, Object());
+    this.videoTime = 0.000;
+    this.toggleEditMode = false;
+    this.songData();
+  }
+
+  public ngOnInit(): void {}
+
+  private songData(): void {
     // @fix change to a variable path 
     let path = '../../assets/json/songs/1_Yasashia.json';
+
     this._songService.getJSON(path).subscribe(
-      res => Object.assign(this.songModel, res),
-      error => console.log(error)
+      response => { 
+        console.log('HTTP response', response);
+        Object.assign(this.chordModel, response);
+      },
+      error => console.log('HTTP Error', error),
+      () => console.log('HTTP request completed.')
     );
-    console.log("Got song: ", this.songModel);
-  }
-  
-  public addTimeEntry(): void {
-    console.log("Clicked add time entry");
   }
 
-  public onChangeSpeed(event): void {
+  public onChangeSpeed(event: any): void {
     this.video.nativeElement.playbackRate = event.target.value;
   }
 
   public onVideoTimeUpdate(): void {
     let currentTime = this.video.nativeElement.currentTime;
-    console.log("Current Video Time", currentTime);
+    this.videoTime = Math.max((Math.round(currentTime * 10) / 1000));
   }
 
+  public onToggleEditMode(): void {
+  }
 }
