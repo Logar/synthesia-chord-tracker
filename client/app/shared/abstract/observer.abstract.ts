4cc012a54e12e989234cb91ee3672b3456ccf611
Ross Arena <rossarena89@gmail.com>
2020-04-06 16:08:12 -0400

    Creates an abstract class for storing a shared observable function between components

---
+++ client/app/shared/abstract/observer.abstract.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observer } from 'rxjs';
+
+@Injectable()
+export abstract class AbstractObserver {
+
+  constructor() { }
+
+  public observable(...callbacks: Array<Function>): Observer<Object> {
+    // Create observer object
+    return {
+      next: response => {
+        callbacks.forEach(f => f.call(this, response))
+      },
+      error: error => console.error('HTTP Error: ', error),
+      complete: () => console.log('Observer got a complete notification')
+    };
+  }
+  
+}
