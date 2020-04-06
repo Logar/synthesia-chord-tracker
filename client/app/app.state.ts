99349fa1022aba83a6de6dfb6eec3571f447cafb
Ross Arena <rossarena89@gmail.com>
2020-04-06 16:13:03 -0400

    Adds console logging to getters and setters in App State

--- client/app/app.state.ts
+++ client/app/app.state.ts
@@ -9,31 +9,37 @@ import { Chord } from './shared/models/chord.model';
 export class AppState {
 
   private _songModels: Song[];
-  private _activeSong: Song;
   private _chordModels: Chord[];
+  private _activeSong: Song;
   
   constructor() {
-    this._activeSong = Object();
     this._songModels = Array();
     this._chordModels = Array();
+    this._activeSong = Object();
   }
 
   get activeSong(): Song {
+    console.log("Active Song Getter: ", this._activeSong);
     return this._activeSong;
   }
   set activeSong(song: Song) {
+    console.log("Active Song Setter: ", song);
     this._activeSong = song;
   }
   get songModels(): Song[] {
+    console.log("Song Models Getter: ", this._songModels);
     return this._songModels;
   }
   set songModels(songs: Song[]) {
+    console.log("Song Models Setter: ", songs);
     this._songModels = songs;
   }
   get chordModels(): Chord[] {
+    console.log("Chord Models Getter: ", this._chordModels);
     return this._chordModels;
   }
   set chordModels(chords: Chord[]) {
+    console.log("Chord Models Setter: ", chords);
     this._chordModels = chords;
   }
 }
