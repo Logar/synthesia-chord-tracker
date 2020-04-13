13437289700ed34546bcd8fa886e9599ecafaf42
Ross Arena <rossarena89@gmail.com>
2020-04-13 15:24:44 -0400

    Sort chords by timestamp in ascending order

--- server/controllers/chord.ts
+++ server/controllers/chord.ts
@@ -7,7 +7,9 @@ export default class ChordCtrl extends BaseCtrl {
   // Get all
   getAll = async (req, res) => {
     try {
-      const docs = await this.model.find({songID: req.params.id});
+      // Find all that matches and sort by timestamp asc
+      const docs = await this.model.find({songID: req.params.id})
+        .sort({'timestamp': 1});
       res.status(200).json(docs);
     } catch (err) {
       return res.status(400).json({ error: err.message });
