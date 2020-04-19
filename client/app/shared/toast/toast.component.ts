ebc2381a99fcce5c0adfc020edc773037d7526e8
Ross Arena <rossarena89@gmail.com>
2020-04-18 21:40:24 -0400

    Defines types for function parameters and fixes this losing scope

--- client/app/shared/toast/toast.component.ts
+++ client/app/shared/toast/toast.component.ts
@@ -8,9 +8,11 @@ import { Component, Input } from '@angular/core';
 export class ToastComponent {
   @Input() message = { body: '', type: '' };
 
-  setMessage(body, type, time = 5000) {
+  setMessage(body: string, type: string) {
+    const self = this;
     this.message.body = body;
     this.message.type = type;
-    setTimeout(() => this.message.body = '', time);
+    // Wait at least 5 seconds before hiding
+    setTimeout(() => self.message.body = '', 5000);
   }
 }
